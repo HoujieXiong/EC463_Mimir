319d95ddeff02bff50e08d741f1225d092a6c965
HoujieXiong <jeffx0506@gmail.com>
2024-11-19 09:32:08 -0500

    validate capture_5s

--- take_pic_5sec.py
+++ take_pic_5sec.py
@@ -1,19 +1,21 @@
 import time
-from picamera import PiCamera
+from picamera2 import Picamera2
 
 # Initialize the camera
-camera = PiCamera()
+camera = Picamera2()
 
+camera.start()
 # Set camera resolution (optional)
-camera.resolution = (1920, 1080)
 
 # Capture images periodically
 try:
     print("Starting periodic image capture. Press Ctrl+C to stop.")
     count = 1  # Image counter for unique filenames
     while True:
-        filename = f"/home/pi/images/image_{count:03d}.jpg"  # Store images in /home/pi/images
-        camera.capture(filename)
+#        filename = f"/home/Visual_AI/Desktop/EC463_Mimir/Images/image_{count:03d}.jpg"  # Store images in /home/pi/images
+        filename = f"/home/Visual_AI/Desktop/EC463_Mimir/Images/image.jpg"  # Store images in /home/pi/images
+
+        camera.capture_file(filename)
         print(f"Captured {filename}")
         count += 1
         time.sleep(5)  # Wait for 5 seconds
